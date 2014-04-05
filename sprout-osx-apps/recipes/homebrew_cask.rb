fe68efd1b6de5a610bc10bd919837949aea18c8a
Jack Viers <jackcviers@gmail.com>
2014-04-05 01:47:14 -0500

    Fix some idempotency issues, more work to be done.

--- sprout-osx-apps/recipes/homebrew_cask.rb
+++ sprout-osx-apps/recipes/homebrew_cask.rb
@@ -2,7 +2,7 @@ include_recipe "sprout-osx-base::homebrew"
 
 execute "tap phinze/homebrew-cask" do
   command "brew tap phinze/homebrew-cask"
-  not_if { system("brew tap | grep 'cask' > /dev/null 2>&1") }
+  not_if do system("brew tap | grep 'cask'") end
 end
 
 package "brew-cask"
